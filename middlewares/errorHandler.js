497920977eed731d711bcfd61a9a685241675f28
Neeraj <sanraj.neeraj@gmail.com>
2023-09-13 10:05:28 +0530

    removed-logger

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,9 +1,9 @@
+const { Console } = require("winston/lib/winston/transports");
 const CustomError = require("../helper/customError");
-const logger = require("../utils/logger");
 
 function handleError(err, req, res, next) {
   if (err instanceof CustomError) {
-    logger.error(`Error Occured : ${err.message}`, { error: err });
+    Console.log(`Error Occured : ${err.message}`, { error: err });
     res.status(err.statusCode).json({ error: err.message });
   } else {
     res.status(500).json({ error: "Internal server error", msg: err.message });
--- server.js
+++ server.js
@@ -2,15 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const itemsRouter = require("./routes/items");
 const handleError = require("./middlewares/errorHandler");
-const logger = require("./utils/logger");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use((req, res, next) => {
-  logger.info(`[${req.method}] ${req.url}`);
-  next();
-});
 app.use("/items", itemsRouter);
 app.use(handleError);
-app.listen(port, () => logger.info("Connected to server on port ", port));
+app.listen(port, () => console.log("Connected to server on port ", port));
